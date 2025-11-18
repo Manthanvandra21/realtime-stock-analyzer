4091d826c961b1fb5219252e2012e36a3e4028d9
Manthanvandra21 <manthanvandra938@gmail.com>
2025-11-18 17:35:32 +0530

    Add functional comments to app1.py
    Added comments outlining the main functionalities of the app.

---
+++ backend/app1.py
@@ -0,0 +1,11 @@
+# File: backend/app.py
+
+# This part will show stock price
+
+# This part will manage portfolio
+
+# This part will check risk
+
+# This part will compare stocks
+
+# This part will show news
